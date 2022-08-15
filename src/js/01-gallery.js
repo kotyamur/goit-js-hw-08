4db516838f7e2b993a36e261ec745c6f6300216d
Kateryna Kleshch <ketrins1993@gmail.com>
2022-08-15 18:39:54 +0300

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const createPhotoGallary = photos => {
+  return photos
+    .map(({ preview, original, description }) => {
+      return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img 
+                    class="gallery__image" 
+                    src="${preview}" 
+                    alt="${description}"
+                    />
+                </a>
+            </div>
+        `;
+    })
+    .join('');
+};
+
+const galleryMarkup = createPhotoGallary(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
