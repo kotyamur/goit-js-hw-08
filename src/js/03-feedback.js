06a04f95456676e38cc9a286e729211d8fadb985
Kateryna Kleshch <ketrins1993@gmail.com>
2022-08-16 13:21:46 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
 
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+const userFeedbackForm = document.querySelector('.feedback-form');
+console.log(userFeedbackForm);
+
+const submitBtn = document.querySelector('form > button')
+console.log(submitBtn);
+
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+
+  console.log('Отправляем форму');
+
+}
+
+const onFormInput = evt => {
+  evt.preventDefault();
+
+  console.log('write');
+};
+userFeedbackForm.addEventListener('submit', onFormSubmit);
+userFeedbackForm.addEventListener('input', throttle(onFormInput, 200));
