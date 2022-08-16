8e8bb4090417690a5fe6e16825949772b5da3a82
Kateryna Kleshch <ketrins1993@gmail.com>
2022-08-16 16:32:37 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,27 +1,45 @@
 import throttle from 'lodash.throttle';
 
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
-console.log(galleryItems);
+const STORAGE_FORM_DATA = 'feedback-form-state';
 
 const userFeedbackForm = document.querySelector('.feedback-form');
-console.log(userFeedbackForm);
+const userEmailLabel =
+  userFeedbackForm.firstElementChild.querySelector('input');
+const userMessageText = userFeedbackForm.querySelector('textarea');
 
-const submitBtn = document.querySelector('form > button')
-console.log(submitBtn);
+const feedbackFormData = {};
+
+const renovateUserInfo = () => {
+  const savedUserFeedback = localStorage.getItem(STORAGE_FORM_DATA);
+  
+  if (savedUserFeedback) {
+    try {
+      const parsedUserFeedback = JSON.parse(savedUserFeedback);
+      userEmailLabel.value = parsedUserFeedback.email;
+      userMessageText.value = parsedUserFeedback.message;
+    } catch (error) {
+      console.log(error.name); // "SyntaxError"
+      console.log(error.message); // "Unexpected token u in JSON at position 1"
+    }
+  }
+}
+
+renovateUserInfo();
 
 const onFormSubmit = (evt) => {
   evt.preventDefault();
-
-  console.log('Отправляем форму');
-
+  
+  console.log(feedbackFormData);
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_FORM_DATA);
 }
 
 const onFormInput = evt => {
   evt.preventDefault();
 
-  console.log('write');
+  feedbackFormData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_FORM_DATA, JSON.stringify(feedbackFormData));
 };
+
 userFeedbackForm.addEventListener('submit', onFormSubmit);
-userFeedbackForm.addEventListener('input', throttle(onFormInput, 200));
+userFeedbackForm.addEventListener('input', throttle(onFormInput, 500));
